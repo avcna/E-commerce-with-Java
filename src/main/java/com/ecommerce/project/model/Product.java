3cdc118aa1be59cdef796b5360e90527dcb4ee10
salsabila.dita <salsabila.dita@admo.astra.co.id>
2026-09-10 21:15:24 +0700

    add authentication

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -3,12 +3,17 @@ package com.ecommerce.project.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +36,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
